6781a98fadf7c081bdfe132cd2d82fde4eba9339
Kippur <marcos.strauss.ort@gmail.com>
2021-09-04 17:13:25 -0300

    Update mainV2.py

--- Python/mainV2.py
+++ Python/mainV2.py
@@ -1,4 +1,5 @@
 # Falta pasar la mayoria de las cosas a funciones !!
+# Cambiar lo del eje x de 100 a 60
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -15,21 +16,30 @@ def make_button(window, x, y, text, command):
     b = tk.Button(window, text=text, command=command)
     b.place(x=x, y=y)
 
-def plot_data(): # FALTA ARREGLAR (No anda)
-    global aux
+def plot_data(): # FALTA ARREGLAR (Anda para el orto) (Igual anda (pero para el orto))
+    global aux, DATA
 
     if aux == True:
-        temp = firebaseBd.get("/Temperatura", "")
+        temp = str(firebaseBd.get("/Temperatura", ""))
 
-        line.set_ydata(temp)
-        #line.set_xdata()
+        if len(DATA) < 100:
+            DATA = np.append(DATA, float(temp[0:4]))
+        
+        else:
+            DATA[0:99] = DATA[1:100]
+            DATA[99] = float(temp[0:4])
+
+        line.set_xdata(np.arange(0,len(DATA)))
+        line.set_ydata(DATA)
+
         canvas.draw()
 
-    window.after(1, plot_data)
+    window.after(1000, plot_data)
 
 def plot_start():
     global aux
     aux = True
+
 
 def plot_stop():
     global aux
@@ -48,7 +58,7 @@ ax1 = fig.add_subplot(111) # Ni idea de porque 111
 ax1.set_title("Titulo")
 ax1.set_xlabel("Sample X")
 ax1.set_ylabel("Sample Y")
-# ax1.set_xlim(0,10) # Tiempo(?)
+ax1.set_xlim(0,100) # Tiempo(?)
 ax1.set_ylim(float(0),float(30)) # Temp
 ax1.spines["top"].set_visible(False)
 ax1.spines["top"].set_visible(False)
@@ -65,5 +75,5 @@ make_button(window, 50, 450, "Start", lambda: plot_start())
 window.update()
 make_button(window, 100, 450, "Stop", lambda: plot_stop())
 
-window.after(1, plot_data)
+window.after(1000, plot_data)
 window.mainloop()
